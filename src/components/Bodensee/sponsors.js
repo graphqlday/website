6bfa4bab734f71e06114713da3d49386da9b6f85
Jesse <themotleydev@gmail.com>
2019-07-19 07:36:22 +0200

    adding sponsors and urls

--- src/components/Bodensee/sponsors.js
+++ src/components/Bodensee/sponsors.js
@@ -36,6 +36,7 @@ export default () => (
         logo {
           url
         }
+        url
       }
 
       {
@@ -44,6 +45,12 @@ export default () => (
             ...Image
           }
           appsync: sponsor(where: { id: "cjwko0z8kb1y10941qkjv5e2o" }) {
+            ...Image
+          }
+          gatsby: sponsor(where: { id: "cjy9n99trdstw0941hlf0qy6u" }) {
+            ...Image
+          }
+          jetbrains: sponsor(where: { id: "cjy9nldk3lybd0d53tbvf34fl" }) {
             ...Image
           }
           honeypot: sponsor(where: { id: "cjuvfq9gpe7uh0c15pf4zkzzw" }) {
@@ -59,20 +66,38 @@ export default () => (
       <Section>
         <section>
           <h2>Organized by</h2>
-          <img style={{maxWidth: 250}} src={data.gcms.graphcms.logo.url} alt="OKgrow logo"
+          <a href={data.gcms.graphcms.url} target="_blank">
+          <img style={{maxWidth: 250}} src={data.gcms.graphcms.logo.url} alt="GraphCMS logo"
           className="padding" />
+          </a>
+          <a href={data.gcms.honeypot.url} target="_blank">
           <img
             src={data.gcms.honeypot.logo.url}
             alt="honeypot logo"
           />
+          </a>
         </section>
         <section>
           <h2>Gold Sponsors</h2>
-          <img src={data.gcms.appsync.logo.url} alt="AWS App-Sync logo" />
+          <a href={data.gcms.appsync.url} target="_blank">
+            <img src={data.gcms.appsync.logo.url} alt="AWS App-Sync logo" />
+          </a>
+        </section>
+        <section>
+          <h2>Bronze Sponsors</h2>
+          <a href={data.gcms.gatsby.url} target="_blank"><img src={data.gcms.gatsby.logo.url} alt="GatsbyJs logo" /></a>
+        </section>
+        <section>
+          <h2>Giveaway Sponsor</h2>
+          <a href={data.gcms.jetbrains.url} target="_blank">
+            <img src={data.gcms.jetbrains.logo.url} alt="Jet Brains logo" style={{maxWidth: 175}}/>
+          </a>
         </section>
         <section>
           <h2>Community Sponsors</h2>
+          <a href={data.gcms.prisma.url} target="_blank">
           <img src={data.gcms.prisma.logo.url} alt="prisma logo" />
+          </a>
         </section>
       </Section>
     )}
